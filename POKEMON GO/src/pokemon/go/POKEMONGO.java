62e9d23f4e8e86222e51874260ff550836781d36
MohaHamra <146078325+MohaHamra@users.noreply.github.com>
2024-04-28 10:26:51 +0200

    Add files via upload

---
+++ POKEMON GO/src/pokemon/go/POKEMONGO.java
@@ -0,0 +1,113 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pokemon.go;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.FitxerCaratula;
+
+/**
+ *
+ * @author Moha
+ */
+public class POKEMONGO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        POKEMONGO app = new POKEMONGO();
+        app.run();
+    }
+    
+    private void run() {
+        String ruta = "fitxers/logo.pok";
+        FitxerCaratula fitxer = new FitxerCaratula(ruta);
+        
+        try {
+            List<String> lineas = fitxer.cargarCaratula();
+            for (String linea : lineas) {
+                System.out.println(linea);
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        boolean exit = false;
+        do{
+            int opcion = Menu();
+            switch(opcion){
+                case 0:
+                    exit=true;
+                    break;
+                case 1:
+                    AltaEntrenador();
+                    break;
+                case 2:
+                    BajaEntrenador();
+                    break;                     
+                case 3:                    
+                    ConsultarEntrenador();
+                    break;  
+                case 4:
+                    CazarPokemon();
+                case 5:
+                    ListarPokemonsCazados();
+                case 6:
+                    ListarPokemonsExsistentes();
+                }   
+            }while(!exit);
+
+    }
+    
+    private int Menu() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] options = {
+            "DAR DE ALTA ENTRENADOR", "DAR DE BAJA ENTRENADOR", "CONSULTAR ENTRENADOR",
+            "CAZAR POKEMON", "LISTAR POKEMONS CAZADOS", "LISTAR POKEMONS EXSISTENTES",};
+        for (int i = 0; i < options.length; i++) {
+            System.out.format("[%d]. %s\n", i, options[i]);
+        }
+        System.out.print("Elige una opcion: ");
+        int op;
+        try {
+            op = Integer.parseInt((reader.readLine()));
+        } catch (NumberFormatException | IOException e) {
+            op = -1;
+        }
+        return op;
+    }
+
+    private void AltaEntrenador() {
+        System.out.println("Has elegido la opcion DAR DE ALTA ENTRENADOR!");
+    }
+
+    private void BajaEntrenador() {
+        System.out.println("Has elegido la opcion DAR DE BAJA ENTRENADOR!");
+    }
+
+    private void ConsultarEntrenador() {
+        System.out.println("Has elegido la opcion DAR DE BAJA ENTRENADOR!");
+    }
+
+    private void ListarPokemonsCazados() {
+        System.out.println("Has elegido la opcion CAZAR POKEMON!");
+    }
+
+    private void CazarPokemon() {
+        System.out.println("Has elegido la opcion LISTAR POKEMONS CAZADOS!");
+    }
+
+    private void ListarPokemonsExsistentes() {
+        System.out.println("Has elegido la opcion LISTAR POKEMONS EXSISTENTES!");
+    }
+    
+}
